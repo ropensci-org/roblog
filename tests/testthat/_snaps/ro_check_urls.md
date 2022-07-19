b7bd06589a6c28023e3e4d2c53575834047aff39
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-07-19 17:03:26 +0200

    improve test

---
+++ tests/testthat/_snaps/ro_check_urls.md
@@ -0,0 +1,8 @@
+# ro_check_urls works
+
+    Code
+      ro_check_urls(path)
+    Message <simpleMessage>
+      Possibly broken URLs: https://masalmon.eu/40004, https://masalmon.eu/400040.
+      Replace http with https for: http://masalmon.eu/.
+
--- tests/testthat/test-ro_check_urls.R
+++ tests/testthat/test-ro_check_urls.R
@@ -1,5 +1,7 @@
 test_that("ro_check_urls works", {
-  path <- system.file(file.path("examples", "bad-no-alt.md"),
-                      package = "roblog")
-  testthat::expect_message(ro_check_urls(path), "Possibly broken URLs: https://masalmon.eu/40004, https://masalmon.eu/400040.")
+  path <- system.file(
+    file.path("examples", "bad-no-alt.md"),
+    package = "roblog"
+  )
+  testthat::expect_snapshot(ro_check_urls(path))
 })
