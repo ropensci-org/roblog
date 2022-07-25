9ce45cfb0f763c6b3fb78c0ce8b48d7171ee6908
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-07-25 10:37:15 +0200

    Remove capitalization checks (#52)

--- tests/testthat/test-ro_lint_md.R
+++ tests/testthat/test-ro_lint_md.R
@@ -4,12 +4,6 @@ test_that("ro_lint_md says all good when no issue", {
   testthat::expect_message(ro_lint_md(path), "All good")
 })
 
-test_that("ro_lint_md find ropensci issues", {
-  path <- system.file(file.path("examples", "bad-no-alt.md"),
-                       package = "roblog")
-  testthat::expect_message(ro_lint_md(path), "lower camelCase")
-})
-
 test_that("ro_lint_md find alt issues", {
   path <- system.file(file.path("examples", "bad-no-alt.md"),
                       package = "roblog")
@@ -26,18 +20,6 @@ test_that("ro_lint_md finds absolute links", {
   path <- system.file(file.path("examples", "bad-no-alt.md"),
                       package = "roblog")
   testthat::expect_message(ro_lint_md(path), "relative links")
-})
-
-test_that("ro_lint_md finds title not in title case", {
-  path <- system.file(file.path("examples", "badtitle.md"),
-                      package = "roblog")
-  testthat::expect_message(ro_lint_md(path), "Title Case")
-})
-
-test_that("ro_lint_md finds headings not in sentence case", {
-  path <- system.file(file.path("examples", "sentencecaseissue.md"),
-                      package = "roblog")
-  testthat::expect_message(ro_lint_md(path), "Sentence case")
 })
 
 test_that("ro_lint_md finds figures not using shortcodes", {
